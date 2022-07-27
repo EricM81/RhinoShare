a18ac892a516dc072eb3ef890124594f892d9a4d
Eric M <emarvets@gmail.com>
2022-07-27 18:56:54 -0400

    added cutter and layout layers; avoid color range

--- Scripts/CreateLayers.py
+++ Scripts/CreateLayers.py
@@ -4,28 +4,49 @@ import Color as c
 
 def createLayers():
     name = rs.GetString("Parent Layer Name")
-    
-    if name:
-        if rs.IsLayer(name) == False:
-            hsv = "lastHue"
-            hue = 0
-            if sc.sticky.has_key(hsv): hue = sc.sticky[hsv]
-            if (hue + 30) == 120:
-                sc.sticky[hsv] = hue+60
-            elif (hue + 30) >= 360:
-                sc.sticky[hsv] = 0
-            else:
-                sc.sticky[hsv] = hue+30
+    geoName = "Default::" + name
+    cutterName = "Cutters::" + name
+    layoutName = "Layout::" + name
+    if geoName:
+        if rs.IsLayer(geoName) == False:
+            #Figure out new layers color structure
+            lastHueKey = "lastHue"
+            lastHue = 0
+
+            if sc.sticky.has_key(lastHueKey): lastHue = sc.sticky[lastHueKey]
             
-            rs.AddLayer(name, c.HSVToColor(hue/360, 1.0, 1.0))
-            rs.CurrentLayer(name)
-            rs.AddLayer(name + "::Control", c.HSVToColor(hue/360, 0.50, 1.0))
-            rs.AddLayer(name + "::Control::Junk", c.HSVToColor(hue/360, 1.0, 0.25))
-            rs.LayerLocked(name + "::Control::Junk", True)
-            rs.AddLayer(name + "::Crvs", c.HSVToColor(hue/360, 1.0, 0.50))
-            rs.AddLayer(name + "::Srf", c.HSVToColor(hue/360, 1.0, 1.0))
+            hue = lastHue + 30
+            if 59 < hue and hue < 149:
+                hue = 160
+            if 360 < hue:
+                hue = 15
+            
+            sc.sticky[lastHueKey] = hue
+            
+            rs.CurrentLayer("Default")
+
+            #create geometry layers
+            rs.AddLayer(geoName, c.HSVToColor(hue/360, 1.0, 1.0))
+            # rs.CurrentLayer(name)
+            rs.AddLayer(geoName, c.HSVToColor(hue/360, 1.0, 1.0))
+            rs.AddLayer(geoName + "::Control", c.HSVToColor(hue/360, 0.50, 1.0))
+            rs.AddLayer(geoName + "::Control::Junk", c.HSVToColor(hue/360, 1.0, 0.25))
+            # rs.LayerLocked(name + "::Control::Junk", True)
+            rs.AddLayer(geoName + "::Crvs", c.HSVToColor(hue/360, 1.0, 0.50))
+            rs.AddLayer(geoName + "::Srf", c.HSVToColor(hue/360, 1.0, 1.0))
+
+            if rs.IsLayer(layoutName) == False:
+                rs.AddLayer(layoutName, c.HSVToColor(hue/360, 1.0, 1.0))
+                rs.AddLayer(layoutName + "::Crvs", c.HSVToColor(hue/360, .15, 0.60))
+                rs.AddLayer(layoutName + "::Srf", c.HSVToColor(hue/360, .15, .30))
+
+            if rs.IsLayer(cutterName) == False:
+                rs.AddLayer(cutterName, c.HSVToColor(hue/360, 1.0, 1.0))
+                rs.AddLayer(cutterName + "::Crvs", c.HSVToColor(120, 1.0, 0.50))
+                rs.AddLayer(cutterName + "::Srf", c.HSVToColor(120, 1.0, .30))
+                
         else:
-            print "ERROR: Layer already exists."
+            print("ERROR: Layer already exists.")
 
 if( __name__ == "__main__" ):
     createLayers()
